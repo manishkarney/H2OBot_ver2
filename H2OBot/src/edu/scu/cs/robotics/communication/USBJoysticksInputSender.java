2b25c6a8189ba7c271ce73ac4410df56f4e8bbe9
manishkarney <kmsamrat1@gmail.com>
2014-04-22 02:33:00 -0700

    Added USB communication.
    Application will be able to send and receive data via USB.
    
    —Issues:
    Not handling the corner cases yet.
    Stability issues.

--- H2OBot/src/edu/scu/cs/robotics/communication/USBJoysticksInputSender.java
+++ H2OBot/src/edu/scu/cs/robotics/communication/USBJoysticksInputSender.java
@@ -4,7 +4,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 /**
- * Runnable that sends information
+ *
  * Created by manishkarney on 4/20/14.
  */
 public class USBJoysticksInputSender{
